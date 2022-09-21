6adb15bc2bdcd82f9a3763be45cf6668c0cd3c0b
Noctis Hsu <noctis.h@binance.com>
2022-09-22 01:46:19 +0800

    feat(scripts): Add brew shells

---
+++ scripts/brew.sh
@@ -0,0 +1,23 @@
+	#!/bin/bash
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew install autojump
+brew install zsh
+brew install wget
+brew install nvim
+brew install fzf
+brew install jq
+brew install exa
+brew install ctop
+brew install vtop
+brew install lazygit
+brew install tig
+brew install tmux
+
+# Remove outdated versions from the cellar
+brew cleanup
