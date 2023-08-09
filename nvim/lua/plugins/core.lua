cd5d6f3113de6bea06fa637dbad0d7018b7d340f
Noctis Hsu <noctis.h@binance.com>
2023-08-09 14:34:37 +0800

    feat(nvim): add lazyvim as nvim configurations

---
+++ nvim/lua/plugins/core.lua
@@ -0,0 +1,157 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {},
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      require("telescope").load_extension("projects")
+    end,
+    keys = {
+      { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+    },
+  },
+  {
+    "echasnovski/mini.starter",
+    optional = true,
+    opts = function(_, opts)
+      local items = {
+        {
+          name = "Projects",
+          action = "Telescope projects",
+          section = string.rep(" ", 22) .. "Telescope",
+        },
+      }
+      vim.list_extend(opts.items, items)
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    optional = true,
+    opts = function(_, dashboard)
+      local button = dashboard.button("p", " " .. " Projects", ":Telescope projects <CR>")
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+      table.insert(dashboard.section.buttons.val, 4, button)
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function()
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs({ "Up", "Down" }) do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require("mini.animate")
+      return {
+        resize = {
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        },
+        scroll = {
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          }),
+        },
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = function()
+      local hi = require("mini.hipatterns")
+      return {
+        -- custom LazyVim option to enable the tailwind integration
+        tailwind = {
+          enabled = true,
+          ft = { "typescriptreact", "javascriptreact", "css", "javascript", "typescript", "html" },
+          -- full: the whole css class will be highlighted
+          -- compact: only the color will be highlighted
+          style = "full",
+        },
+        highlighters = {
+          hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+        },
+      }
+    end,
+    config = function(_, opts)
+      -- backward compatibility
+      if opts.tailwind == true then
+        opts.tailwind = {
+          enabled = true,
+          ft = { "typescriptreact", "javascriptreact", "css", "javascript", "typescript", "html" },
+          style = "full",
+        }
+      end
+      if type(opts.tailwind) == "table" and opts.tailwind.enabled then
+        -- reset hl groups when colorscheme changes
+        vim.api.nvim_create_autocmd("ColorScheme", {
+          callback = function()
+            M.hl = {}
+          end,
+        })
+        opts.highlighters.tailwind = {
+          pattern = function()
+            if not vim.tbl_contains(opts.tailwind.ft, vim.bo.filetype) then
+              return
+            end
+            if opts.tailwind.style == "full" then
+              return "%f[%w:-]()[%w:-]+%-[a-z%-]+%-%d+()%f[^%w:-]"
+            elseif opts.tailwind.style == "compact" then
+              return "%f[%w:-][%w:-]+%-()[a-z%-]+%-%d+()%f[^%w:-]"
+            end
+          end,
+          group = function(_, _, m)
+            ---@type string
+            local match = m.full_match
+            ---@type string, number
+            local color, shade = match:match("[%w-]+%-([a-z%-]+)%-(%d+)")
+            shade = tonumber(shade)
+            local bg = vim.tbl_get(M.colors, color, shade)
+            if bg then
+              local hl = "MiniHipatternsTailwind" .. color .. shade
+              if not M.hl[hl] then
+                M.hl[hl] = true
+                local bg_shade = shade == 500 and 950 or shade < 500 and 900 or 100
+                local fg = vim.tbl_get(M.colors, color, bg_shade)
+                vim.api.nvim_set_hl(0, hl, { bg = "#" .. bg, fg = "#" .. fg })
+              end
+              return hl
+            end
+          end,
+          priority = 2000,
+        }
+      end
+      require("mini.hipatterns").setup(opts)
+    end,
+  },
+}
